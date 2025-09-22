2ae5d0aba959af6fb79b792ef4a0a64b8362477d
Summiya Aurangzeb <summiya.bese25seecs@seecs.edu.pk>
2025-09-22 22:23:32 +0500

    Add files via upload

---
+++ Variables and Datatypes.py
@@ -0,0 +1,10 @@
+#defining variables
+name = "Summiya"
+age = 18
+height = 5.4
+is_student = True
+#print
+print("My Name is: ",name)
+print("My Age is: ",age)
+print("My Height is: ",height)
+print("Am I a student: ",is_student)
